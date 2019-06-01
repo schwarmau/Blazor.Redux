ca2d5d53aee5f7721f750deccdbeaf3f96dad653
Austin Schwarm <austin.schwarm@uconn.edu>
2019-06-01 08:40:17 -0400

    refactored AddReduxStore to behave more like similar operations native to .NET Core and made the store scoped in the server library

--- Blazor.Server.Redux/ExtensionMethods.cs
+++ Blazor.Server.Redux/ExtensionMethods.cs
@@ -1,26 +1,29 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Blazor.Server.Redux
 {
     public static class ExtensionMethods
     {
         /// <summary>
-		/// Add a store interface singleton to the application's service collection.
+		/// Add a store singleton to the application's service collection.
 		/// </summary>
 		/// <typeparam name="TState">The type of the state.</typeparam>
 		/// <typeparam name="TAction">The type of action that the state's reducers take.</typeparam>
 		/// <param name="initialState">The initial state.</param>
 		/// <param name="rootReducer">The top-level reducer for the state.</param>
-		/// <param name="options">Options determining how redux handles certain functions.</param>
+		/// <param name="configure">Options configuration determining how redux handles certain functions.</param>
 		/// <returns>The singleton store assigned.</returns>
         public static Store<TState, TAction> AddReduxStore<TState, TAction>(
-            this IServiceCollection configure,
+            this IServiceCollection services,
             TState initialState,
             Reducer<TState, TAction> rootReducer,
-            ReduxOptions<TState, TAction> options = null)
+            Action<ReduxOptions<TState, TAction>> configure = null)
         {
-            var store = new Store<TState, TAction>(initialState, rootReducer, options);
-            configure.AddSingleton<Store<TState, TAction>>(store);
+            ReduxOptions<TState, TAction> options = new ReduxOptions<TState, TAction>();
+            configure?.Invoke(options);
+            Store<TState, TAction> store = new Store<TState, TAction>(initialState, rootReducer, options);
+            services.AddScoped<Store<TState, TAction>>(sp => store);
             return store;
         }
     }
