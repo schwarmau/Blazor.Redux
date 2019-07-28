e1885ba88adf8faa509abec7b97d2eb67d3e2c9a
Matthias Winkler <mwinkler@gmx.ch>
2019-07-28 22:43:09 +0200

    Allow reducer to be resolved via dependency injection

--- Blazor.Server.Redux/ExtensionMethods.cs
+++ Blazor.Server.Redux/ExtensionMethods.cs
@@ -11,18 +11,18 @@ namespace Blazor.Server.Redux
 		/// <typeparam name="TState">The type of the state.</typeparam>
 		/// <typeparam name="TAction">The type of action that the state's reducers take.</typeparam>
 		/// <param name="initialState">The initial state.</param>
-		/// <param name="rootReducer">The top-level reducer for the state.</param>
+		/// <param name="rootReducer">The top-level reducer for the state. If not given, its resolved via DI.</param>
 		/// <param name="configure">Options configuration determining how redux handles certain functions.</param>
 		/// <returns>The singleton store assigned.</returns>
         public static void AddReduxStore<TState, TAction>(
             this IServiceCollection services,
             TState initialState,
-            Reducer<TState, TAction> rootReducer,
+            Reducer<TState, TAction> rootReducer = null,
             Action<ReduxOptions<TState, TAction>> configure = null)
         {
             ReduxOptions<TState, TAction> options = new ReduxOptions<TState, TAction>();
             configure?.Invoke(options);
-            services.AddScoped(sp => new Store<TState, TAction>(initialState, rootReducer, options));
+            services.AddScoped(sp => new Store<TState, TAction>(initialState, rootReducer ?? sp.GetService<Reducer<TState, TAction>>(), options));
         }
     }
 }
